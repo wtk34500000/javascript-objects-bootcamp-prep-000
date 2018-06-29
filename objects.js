173fce92d74f36785046814ae95d06f3c93eec7e
weijun huang <luck938938@hotmail.com>
2018-06-29 04:16:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = new Object({artist: 'song title'})
 
 function updatePlaylist(playlist, artistName, songTitle)
@@ -11,3 +12,6 @@ function removeFromPlaylist(playlist, artistName)
   delete playlist[artistName]
   return playlist
 }
+=======
+var playlist = new Object({})
+>>>>>>> 1ffd34b8ec25410993e482897aa05bcc009aa4cf
